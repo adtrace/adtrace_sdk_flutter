f54f220c6569478f37c264d723e7a8d133421dd2
Nasser Amini <namini40@gmail.com>
2023-01-02 09:36:37 +0330

    update `version`

--- lib/adtrace.dart
+++ lib/adtrace.dart
@@ -16,7 +16,7 @@ import 'package:flutter/services.dart';
 import 'package:meta/meta.dart';
 
 class AdTrace {
-  static const String _sdkPrefix = 'flutter1.2.1+2';
+  static const String _sdkPrefix = 'flutter1.3.0';
   static const MethodChannel _channel =
       const MethodChannel('io.adtrace.sdk/api');
 
