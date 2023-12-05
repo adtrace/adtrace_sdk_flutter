dd13a8e3844222e0623c79660e995be3f0afb92f
Nasser Amini <namini40@gmail.com>
2023-12-05 17:46:11 +0330

    refactor(example): update example android module

--- example/android/app/src/main/java/io/adtrace/sdk/flutter/adtrace_sdk_flutter_example/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package io.adtrace.sdk.flutter.adtrace_sdk_flutter_example;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
---
+++ example/android/app/src/main/kotlin/io/adtrace/examples/MainActivity.kt
@@ -0,0 +1,6 @@
+package io.adtrace.examples
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
